02da4a148ea83c48de56ffef35b55dc5030c5f7d
ymixteco <ymixteco@iwa.com.mx>
2026-09-21 16:05:07 -0600

    feat: add foreign key constraint for instructor_id in course table

--- src/main/resources/db/migration/V2026.09.17.11.19__alter_course_add_instructor_and_unique_name.sql
+++ src/main/resources/db/migration/V2026.09.17.11.19__alter_course_add_instructor_and_unique_name.sql
@@ -1,6 +1,10 @@
 ALTER TABLE course
-    ADD COLUMN instructor_id UUID NOT NULL
-        REFERENCES instructor(id);
+    ADD COLUMN instructor_id UUID NOT NULL;
+
+ALTER TABLE course
+    ADD CONSTRAINT courses_instructor_id_fk
+        FOREIGN KEY (instructor_id)
+            REFERENCES instructor(id);
 
 
 ALTER TABLE course ADD CONSTRAINT course_name_uk UNIQUE (name);
